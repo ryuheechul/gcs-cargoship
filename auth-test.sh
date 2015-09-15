ca76246c79a837dee8c64a4361ab9e4517d04bad
Heechul Ryu <ryuhcii@gmail.com>
2015-09-15 13:39:38 +0900

    changed image name from python-gsutil to gcs-cargoship.

--- auth-test.sh
+++ auth-test.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-docker build -t python-gsutil .
+./build.sh
 
 source .env
-docker run -it \
+docker run -t \
 	-e GCS_BUCKET_NAME="$GCS_BUCKET_NAME" \
 	-e GC_SECRET_PATH="$GC_SECRET_PATH" \
 	-e GC_SECRET_PASSWORD="$GC_SECRET_PASSWORD" \
 	-e GC_SERVICE_ACCOUNT="$GC_SERVICE_ACCOUNT" \
 	-e GC_PROJECT_ID="$GC_PROJECT_ID" \
-	python-gsutil gsutil ls
-
+	gcs-cargoship gsutil ls
